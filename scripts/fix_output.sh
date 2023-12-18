26b99143780cd4e133cab77c324dd3820d550c31
Pathogen Genomics, Wellcome Trust Sanger Institute <psu-dev@sanger.ac.uk>
2023-12-18 14:01:58 +0000

    adapt to pahse 3 data

--- scripts/fix_output.sh
+++ scripts/fix_output.sh
@@ -5,5 +5,5 @@ path_to_taxonomy_lookup="/data/pam/team230/wr7/scratch/rvi/instrain/dbs/viral_re
 declare -A seq_id_to_name; while read key value; do seq_id_to_name["$key"]="$value"; done < "${path_to_taxonomy_lookup}"
 
 for file in *.tsv; do
-  paste -d '\t' <(echo genome; while read key; do echo ${seq_id_to_name["$key"]};done < <(tail +2 "$file" | sed "s/.*viral.1.1.genomic_sorted.id_\(.*\).fna.*/\1/g")) <(awk -F $'\t' '{for (i=2; i<NF; i++) printf $i "\t"; print $NF}' "$file") > "${file%.tsv}_fixed.tsv"
+  paste -d '\t' <(echo genome; while read key; do echo ${seq_id_to_name["$key"]};done < <(tail +2 "$file" | sed "s/.*viral.1.1.genomic.id_\(.*\).fna.*/\1/g")) <(awk -F $'\t' '{for (i=2; i<NF; i++) printf $i "\t"; print $NF}' "$file") > "${file%.tsv}_fixed.tsv"
 done
