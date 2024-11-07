92090dfa9b70ad9e39e3dc944233b54bcdd5dd31
Florent Lassalle <fl4@sanger.ac.uk>
2024-11-07 16:37:27 +0000

    add shellcheck exemption directive

--- templates/subset_fasta.sh
+++ templates/subset_fasta.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# shellcheck disable=SC1083,SC2034,SC2154
+# above directive prevents shellcheck to report use of
+# SC1083: litteral syntax \${}, required as this is a template script
+# SC2034: apparently unused variables, which derives from the above
+# SC2154: variables referenced but apparently not assigned; this results from nextflow variables being injected into the bash code
 while read genome
     do
       genome_split_to_path=`echo "\${genome}" | sed -e 's|\\(GC[AF]\\)_\\([0-9]\\{3\\}\\)\\([0-9]\\{3\\}\\)\\([0-9]\\{3\\}\\)\\.[0-9]|\\1/\\2/\\3/\\4|'`
