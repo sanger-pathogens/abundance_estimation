25447a0a753a4a1e934dcdebc376ec44802c4c16
Isabelle Coates <ic10@sanger.ac.uk>
2025-06-25 11:51:00 +0100

    Update subset_fasta to allow subset_ref_data to be generated from Instrain bactrial_protozoa_viral_fungi database

--- templates/subset_fasta.sh
+++ templates/subset_fasta.sh
@@ -5,17 +5,24 @@
 # SC2034: apparently unused variables, which derives from the above as variables are being called with litteral syntax 
 # SC2050 : expressions appear constant, which derives from the above as variables are being called with litteral syntax
 # SC2154: variables referenced but apparently not assigned; this results from nextflow variables being injected into the bash code
-while read genome
+while read genome; 
     do
+    
       genome_split_to_path=`echo "\${genome}" | sed -e 's|\\(GC[AF]\\)_\\([0-9]\\{3\\}\\)\\([0-9]\\{3\\}\\)\\([0-9]\\{3\\}\\)\\.[0-9]|\\1/\\2/\\3/\\4|'`
       genome_file_path="${genome_dir}/database/\${genome_split_to_path}/\${genome}${genomes_file_ext}"
       [ -e \${genome_file_path} ] || genome_file_path="${genome_dir}/\${genome}${genomes_file_ext}"
 
-      if [[ "\${genome_file_path}" =~ s3:.* ]] # S3 mount
-      then
-        ${aws_cli} s3 --no-progress cp \${genome_file_path} - | zcat >> subset_ref_database.fasta
-      else # File system
-        zcat -f \${genome_file_path} >> subset_ref_database.fasta
-      fi
+
+    if [ ! -e "$genome_file_path" ]; then
+        if [ -f "indexed_genomes.txt" ]; then
+            genome_file_path=$(grep "^${genome}@" "indexed_genomes.txtß" | cut -d '@' -f2)
+        fi
+    fi
+
+    if [[ "$genome_file_path" =~ ^s3:.* ]]; then
+        ${aws_cli} s3 --no-progress cp "$genome_file_path" - | zcat >> subset_ref_database.fasta
+    else
+        zcat -f "$genome_file_path" >> subset_ref_database.fasta
+    fi
 
 done < ${sourmash_genomes}
