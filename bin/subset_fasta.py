ecec6b0c2f11123235b9d703a4f1f378abe4ad30
Sahra Musse <sm71@sanger.ac.uk>
2026-02-20 12:37:31 +0000

    Handle non-gz genomes in subset_fasta

--- bin/subset_fasta.py
+++ bin/subset_fasta.py
@@ -11,6 +11,11 @@ import re
 import gzip
 import shutil
 
+def open_maybe_gzip(path):
+    if path.endswith(".gz"):
+        return gzip.open(path, "rb")
+    return open(path, "rb")
+
 
 def subset_fasta(genome_dir, sourmash_genomes):
 
@@ -24,35 +29,58 @@ def subset_fasta(genome_dir, sourmash_genomes):
         # Iterate through each genome file name in the sourmash file
         for genome_file_name in sourmash_file: 
             genome_file_name = genome_file_name.strip()
+            genome_file_path = None
+
             # Only execute if directory is in a GCF pattern
             if not_GCF_pattern:
                 # Create the path for the genome file based on the GCF pattern
                 pattern_re = r"(^[A-Z]{3})_([0-9]{3})([0-9]{3})([0-9]{3}).*$"
-                genome_split_to_path = re.sub(pattern_re, r"database/\1/\2/\3/\4/", genome_file_name)
-                genome_file_path = os.path.join(genome_dir, genome_split_to_path, genome_file_name)
-                print("file path: ", genome_file_path)
-            if os.path.exists(genome_file_path) and not_GCF_pattern:
+                if re.match(pattern_re, genome_file_name):
+                    genome_split_to_path = re.sub(pattern_re, r"database/\1/\2/\3/\4/", genome_file_name)
+                    genome_file_path = os.path.join(genome_dir, genome_split_to_path, genome_file_name)
+                    print("file path: ", genome_file_path)
+
+            if genome_file_path and os.path.exists(genome_file_path) and not_GCF_pattern:
                 try:
-                    with gzip.open(genome_file_path, 'rb') as genome_file:
+                    with open_maybe_gzip(genome_file_path) as genome_file:
                         shutil.copyfileobj(genome_file, subset_file)
                 except FileNotFoundError as e:
                     print(f"Error {genome_file_path} not found: {e}")
                 except BaseException as e:
                     print(f"Error processing {genome_file_path}: {e}")
+            elif genome_file_path and os.path.exists(genome_file_path + ".gz") and not_GCF_pattern:
+                try:
+                    with open_maybe_gzip(genome_file_path + ".gz") as genome_file:
+                        shutil.copyfileobj(genome_file, subset_file)
+                except FileNotFoundError as e:
+                    print(f"Error {genome_file_path}.gz not found: {e}")
+                except BaseException as e:
+                    print(f"Error processing {genome_file_path}.gz: {e}")
             else:
                 # If the directory is not in a GCF pattern, search for the genome file in the genome directory
                 not_GCF_pattern = False
-                print("file path2: ", genome_file_path)
                 for root, _, files in os.walk(genome_dir):
                     if genome_file_name in files:
                         genome_file_path = os.path.join(root, genome_file_name)
                         try:
-                            with gzip.open(genome_file_path, 'rb') as genome_file:
+                            with open_maybe_gzip(genome_file_path) as genome_file:
                                 shutil.copyfileobj(genome_file, subset_file)
                         except FileNotFoundError as e:
                             print(f"Error {genome_file_path} not found: {e}")
                         except BaseException as e:
                             print(f"Error processing {genome_file_path}: {e}")
+                        break
+                    if genome_file_name + ".gz" in files:
+                        genome_file_path = os.path.join(root, genome_file_name + ".gz")
+                        try:
+                            with open_maybe_gzip(genome_file_path) as genome_file:
+                                shutil.copyfileobj(genome_file, subset_file)
+                        except FileNotFoundError as e:
+                            print(f"Error {genome_file_path} not found: {e}")
+                        except BaseException as e:
+                            print(f"Error processing {genome_file_path}: {e}")
+                        break
+
 
 
 if __name__ == "__main__":
